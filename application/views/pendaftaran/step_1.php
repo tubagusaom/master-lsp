601a7e61b127f2165c0d1752f2e893d976587451
Tera Byte <tubagus.aom.swk@gmail.com>
2022-09-25 01:35:32 +0700

    mergenew

---
+++ application/views/pendaftaran/step_1.php
@@ -0,0 +1,84 @@
+<section>
+  <div class="inner">
+
+    <div class="wizard-header">
+      <h3 class="heading">SKEMA SERTIFIKASI</h3>
+      <p>Silahkan pilih salah satu skema yang akan diujikan </p>
+    </div>
+
+    <div class="panel-group" id="accordiontb">
+
+      <div class="panel panel-default">
+
+        <a data-toggle="collapse" data-parent="#accordiontb" href="#collapseOne">
+          <div class="panel-heading">
+            <h4 class="panel-title accordion-toggle">
+              #1 Skema Satu
+            </h4>
+          </div>
+        </a>
+
+        <div id="collapseOne" class="panel-collapse collapse">
+          <div class="panel-body card">
+            <div class="card-body">
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex align-items-center">
+                  <div class="ms-3">
+                    <p class="text-muted mb-0">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card-footer bg-light p-2 d-flex justify-content-around">
+              <a class="btn btn-link m-0 text-reset " href="#" role="button" data-ripple-color="primary">
+                Download <i class="fa fa-download ms-2"></i>
+              </a>
+              <a class="btn btn-link m-0 text-reset" href="#" role="button" data-ripple-color="primary">
+                Pilih Skema <i class="fa fa-check-square-o ms-2"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="panel panel-default">
+        <a data-toggle="collapse" data-parent="#accordiontb" href="#collapseTwo">
+          <div class="panel-heading">
+            <h4 class="panel-title accordion-toggle">
+              #2 Skema Dua
+            </h4>
+          </div>
+        </a>
+
+        <div id="collapseTwo" class="panel-collapse collapse">
+          <div class="panel-body card">
+            <div class="card-body">
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex align-items-center">
+                  <div class="ms-3">
+                    <p class="text-muted mb-0">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card-footer bg-light p-2 d-flex justify-content-around">
+              <a class="btn btn-link m-0 text-reset " href="#" role="button" data-ripple-color="primary">
+                Download <i class="fa fa-download ms-2"></i>
+              </a>
+              <a class="btn btn-link m-0 text-reset" href="#" role="button" data-ripple-color="primary">
+                Pilih Skema <i class="fa fa-check-square-o ms-2"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+  </div>
+</section>
---
+++ application/views/pendaftaran/ujikom.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Pendaftaran | <?=$aplikasi->singkatan_unit?></title>
+
+	<link rel="shortcut icon" href="<?=base_url()?>assets/icon-pendaftaran.png" type="image/x-icon" />
+
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Font-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/_tera_byte/form/css/opensans-font.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/_tera_byte/form/fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
+
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/bootstraps/font-awesome.min.css" type="text/css"/>
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/_tera_byte/form/vendor/bootstrap.3.3.4/css/bootstrap.min.css">
+
+	<!-- Main Style Css -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/_tera_byte/form/css/style.css"/>
+
+	<!-- Main Style tera_byte -->
+	   <link rel="stylesheet" href="<?php echo base_url() ?>assets/_tera_byte/form/css/wizard.css"/>
+ 		 <link rel="stylesheet" href="<?php echo base_url() ?>assets/_tera_byte/form/css/accordion.css"/>
+
+
+		 <style media="screen">
+
+		 </style>
+
+</head>
+
+<body>
+
+
+
+	<div class="page-content">
+
+		<div id="formHeader" class="form-header">
+			<div class="wizard-header-2">
+				<ul class="wizard2">
+				  <li class="active2">
+				    <span>Skema</span>
+				  </li>
+				  <li>
+				    <span>APL-01</span>
+				  </li>
+				  <li>
+				    <span>Dokumen</span>
+				  </li>
+				  <li>
+				    <span>APL-02</span>
+				  </li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="form-v1-content">
+			<div class="wizard-form">
+			      <form id="formRegister" class="form-register" action="#" method="post">
+
+							<!-- <div class="logo-form">
+								<a href="<?=base_url()?>" title="Kembali ke beranda">
+									<font><i class="fa fa-home"></i></font>
+									<img src="<?php echo base_url() ?>assets/img/logo1_transparent.png" alt="tera_byte">
+								</a>
+							</div> -->
+
+			        <div id="form-total">
+
+									<h2>
+										<p class="step-icon"><span>01</span></p>
+										<span class="step-text">Skema Sertifikasi</span>
+									</h2>
+									<?php
+										$this->load->view('pendaftaran/step_1');
+									?>
+
+			            <h2>
+			              <p class="step-icon"><span>02</span></p>
+			              <span class="step-text">Personal Infomation</span>
+			            </h2>
+			            <?php
+			              $this->load->view('pendaftaran/step_2');
+			            ?>
+
+			            <h2>
+			              <p class="step-icon"><span>03</span></p>
+			              <span class="step-text">Connect Bank Account</span>
+			            </h2>
+			            <?php
+			              $this->load->view('pendaftaran/step_3');
+			            ?>
+
+			            <h2>
+			              <p class="step-icon"><span>04</span></p>
+			              <span class="step-text">Set Financial Goals</span>
+			            </h2>
+			            <?php
+			              $this->load->view('pendaftaran/step_4');
+			            ?>
+
+			        </div>
+			      </form>
+			</div>
+
+		</div>
+
+
+	</div>
+
+	<script>
+
+		window.onscroll = function() {myFunction()};
+
+		var header = document.getElementById("formHeader");
+
+		// var header = $('#formHeader');
+		var sticky = header.offsetTop;
+
+		function myFunction() {
+		  if (window.pageYOffset > sticky) {
+		    header.classList.add("sticky");
+				header.classList.add("shadow1");
+				header.classList.remove("shadow2");
+		  } else {
+		    header.classList.remove("sticky");
+				header.classList.remove("shadow1");
+				header.classList.add("shadow2");
+		  }
+		}
+	</script>
+
+	<!-- jQuery -->
+	<script src="<?php echo base_url() ?>assets/_tera_byte/form/vendor/jquery.2.1.3/jquery.min.js"></script>
+	<!-- Bootstrap JS -->
+	<script src="<?php echo base_url() ?>assets/_tera_byte/form/vendor/bootstrap.3.3.4/js/bootstrap.min.js"></script>
+
+	<!-- <script src="<?php echo base_url() ?>assets/_tera_byte/form/js/jquery-3.3.1.min.js"></script> -->
+	<script src="<?php echo base_url() ?>assets/_tera_byte/form/js/jquery.steps.js"></script>
+	<script src="<?php echo base_url() ?>assets/_tera_byte/form/js/main.js"></script>
+
+
+
+</body>
+</html>
